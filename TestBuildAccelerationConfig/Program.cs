9300ec13ad532093a9fc5bb2aadca640eadd0d0c
Andrew Stevenson <Andrew.Stevenson@lifetimetraining.co.uk>
2023-03-21 17:58:31 +0000

    Add project files.

---
+++ TestBuildAccelerationConfig/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+
+        IConfiguration config = builder.Build();
+
+        Console.WriteLine(config.GetValue<string>("config1"));
+        Console.WriteLine(config.GetValue<string>("config2"));
+    }
+}
